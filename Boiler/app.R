639a939b353771aa5192c7ed0bc2cdda3e36acb0
rr1964 <rreese531@gmail.com>
2021-07-07 14:38:51 -0600

    Add files via upload

---
+++ Boiler/app.R
@@ -0,0 +1,18 @@
+
+#
+
+library(shiny)
+
+# Define UI 
+ui <- fluidPage(
+
+)
+
+# Define server logic
+server <- function(input, output) {
+
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)

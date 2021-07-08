8109b86b938c352838487982f0b6fb87bf2eab66
rr1964 <rreese531@gmail.com>
2021-07-08 13:27:39 -0600

    Create BoilerPlate.py

---
+++ Dash/BoilerPlate.py
@@ -0,0 +1,25 @@
+# Run this app with `python BoilerPlate.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+app = dash.Dash()
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash boilerplate.
+    '''),
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
